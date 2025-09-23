fe195ede10d16bd493b891c52719907944a133e7
alfiqpcr <alfiq24si@mahasiswa.pcr.ac.id>
2025-09-23 07:51:58 +0700

    setup awal projek

---
+++ resources/views/halaman-index-matakuliah.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <h1>Selamat Datang!</h1>
+        <h2>Ini halaman index matakuliah
+            <p> update
+                delete
+                hapus</p>
+        </h2>
+    </body>
+</html>
