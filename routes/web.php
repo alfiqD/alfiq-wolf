61f3d171ad1eb0bb583e2b7c44afbf63d48bf422
alfiqD <alfiq24si@mahasiswa.pcr.ac.id>
2025-10-07 10:35:27 +0700

    pasang bootstrap

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MatakuliahController;
 
@@ -43,3 +44,6 @@ Route::get('/pegawai', [PegawaiController::class, 'index']);
 
 Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
+
+
+Route::get('dashboard', [DashboardController::class,'index'])->name('dashboard');
